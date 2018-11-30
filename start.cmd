2202c8cd46547e5c2776db172547f648213311a7
vmlankub <45290401+vmlankub@users.noreply.github.com>
2018-11-30 22:15:12 +0800

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,1 +1,1 @@
-xmrig.exe -B --donate-level 1 --cpu-priority 3 -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -p xx -k
+xmrig.exe --donate-level 1 --cpu-priority 3 -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -p xx -k
