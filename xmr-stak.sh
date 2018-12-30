6af081f92345c87f672f493b7a1abd5f7151fc2f
vmlankub <45290401+vmlankub@users.noreply.github.com>
2018-12-30 13:31:24 +0800

    Update xmr-stak.sh

--- xmr-stak.sh
+++ xmr-stak.sh
@@ -6,5 +6,5 @@ mkdir xmr-stak/build
 cd xmr-stak/build
 cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF -DMICROHTTPD_ENABLE=OFF -DHWLOC_ENABLE=OFF -DOpenSSL_ENABLE=OFF
 make install
-./bin/xmr-stak -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -p x --currency monero
+./bin/xmr-stak -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -r  -p x --currency monero
 # chmod +x xmr-stak.sh
