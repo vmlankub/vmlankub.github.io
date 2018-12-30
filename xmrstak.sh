676b6db9763a7920da07ce6b2305cc07be4457e4
vmlankub <45290401+vmlankub@users.noreply.github.com>
2018-12-30 13:47:17 +0800

    Update xmrstak.sh

--- xmrstak.sh
+++ xmrstak.sh
@@ -2,4 +2,4 @@
 wget https://github.com/fireice-uk/xmr-stak/releases/download/2.7.1/xmr-stak-linux-2.7.1-cpu_cuda-nvidia.tar.xz
 tar xvJf  xmr-stak-linux-2.7.1-cpu_cuda-nvidia.tar.xz
 screen -S xx
-./xmr-stak-linux-2.7.1-cpu_cuda-nvidia/xmr-stak -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -r  -p x --currency monero
+./xmr-stak-linux-2.7.1-cpu_cuda-nvidia/xmr-stak -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -p x --currency monero
